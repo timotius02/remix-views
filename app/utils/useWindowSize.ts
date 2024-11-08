617560f319ab7898e0c4a93a80a1033da6dc2019
Timotius Sitorus <timotius@kalibrr.com>
2024-11-08 23:17:48 +0700

    clean up code

--- app/utils/useWindowSize.ts
+++ app/utils/useWindowSize.ts
@@ -10,14 +10,14 @@ const useWindowSize = () => {
     height: 0,
   });
 
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }
   useEffect(() => {
     if (typeof window !== "undefined") {
-      function handleResize() {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
 
       window.addEventListener("resize", handleResize);
 
